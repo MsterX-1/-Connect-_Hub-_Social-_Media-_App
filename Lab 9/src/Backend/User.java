9bd2b3d9f45b489828a88709de68ca97ed022d86
Youssef Ibrahim <es-youssef.taawash2027@alexu.edu.eg>
2024-12-02 15:27:56 +0200

    User class add

---
+++ Lab 9/src/Backend/User.java
@@ -0,0 +1,69 @@
+package Backend;
+
+import java.time.LocalDate;
+
+public class User {
+    private String userId;
+    private String email;
+    private String password;
+    private String username;
+    private LocalDate dateOfBirth;
+    private boolean status;
+
+    public User(String userId, String email, String password, String username, LocalDate dateOfBirth, boolean status) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.dateOfBirth = dateOfBirth;
+        this.status = status;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
